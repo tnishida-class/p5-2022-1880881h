8dd798fa7e8cb4b651021969ad5e302a3a19a135
1880881h <115515285+1880881h@users.noreply.github.com>
2022-10-13 13:04:18 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -6,13 +6,15 @@ background(255,255,255);
     
 if(i<5){
 let x = 100 - (10 *(i-1) );
+stroke(255, 0, 0);
 ellipse(50, 50, x);
-stroke(255, 0, 0);}
+}
   
 else if (5 < i <11 ){
 let x = 100 - (10 *(i-1) );
-ellipse(50, 50, x);
 stroke(0, 0, 255);
+ellipse(50, 50, x);
+
 }
 
 }
