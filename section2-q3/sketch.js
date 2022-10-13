3c8ae7fde330e6209a3f82b3e2e748bf186fe0bc
1880881h <115515285+1880881h@users.noreply.github.com>
2022-10-13 16:42:18 +0900

    Update sketch.js

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -1,35 +1,78 @@
-// ダーツ
-
-function setup() {
-  const green = color(0, 255, 0);
-  const red = color(255, 0, 0);
-  const black = color(0);
-  const cream = color(242, 212, 147);
-  createCanvas(400, 400);
-  background(255);
-  stroke(255);
-  strokeWeight(3);
-
-  const cx = width / 2; // 中心は (cx, cy)
-  const cy = height / 2;
-  const maxR = min(width, height); // 大きさは幅と高さのうち小さい方
-
-  drawCircle(black, maxR);
-  drawArcs(green, red, maxR * 0.8);
-  // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
-  drawCircle(red, maxR * 0.05);
-}
-
-function drawCircle(c, r){
-  fill(c);
-  ellipse(cx, cy, r, r);
-}
-
-function drawArcs(c1, c2, r) {
-  for (let i = 0; i < 20; i++) {
-    let start = TWO_PI / 20 * i;
-    let stop = TWO_PI / 20 * (i + 1);
-    fill(i % 2 == 0 ? c1 : c2);
-    arc(cx, cy, r, r, start, stop, PIE);
+/function setup(){
+    createCanvas(500, 500);
+    background(255);
   }
+
+  function draw(){
+
+//円1：黒
+　stroke(255);
+    fill(0);
+    ellipse(250, 250,350);
+
+    for(let i = 0; i < 20; i++){
+    
+    let rate1 = (i-1) / 20;
+    let rate2 = i / 20;
+
+let angle1 = TWO_PI * rate1;
+    let angle2 = TWO_PI * rate2
+    let n = i % 2
+    console.log(i, rate1 , rate2 , angle1 , angle2 ,n  );
+
+   if(n == 1){
+    stroke(255);
+
+　//円2：緑
+    fill(0,84,56);
+    arc(250, 250, 270, 270, angle1, angle2 ,PIE );
+
+　//円3：ベージュ
+    fill(231,208,169);
+    arc(250, 250, 250, 250, angle1, angle2 ,PIE );
+
+　//円4：緑
+    fill(0,84,56);
+    arc(250, 250, 170, 170, angle1, angle2 ,PIE );
+
+   //円5：ベージュ
+    fill(231,208,169);
+    arc(250, 250, 150, 150, angle1, angle2 ,PIE );
+
+    }
+
+    
+    else if(n == 0){
+    stroke(255);
+
+    //円2：赤
+    fill(192,0,0);
+    arc(250, 250, 270, 270, angle1, angle2 ,PIE );
+
+　//円３：黒
+    fill(0);
+    arc(250, 250, 250, 250, angle1, angle2 ,PIE );
+
+    //円4：赤
+    fill(192,0,0);
+    arc(250, 250, 170, 170, angle1, angle2 ,PIE );
+
+    //円5：黒
+    fill(0);
+    arc(250, 250, 150, 150, angle1, angle2 ,PIE );
+    }
+
+//円6：緑
+　stroke(255);
+    fill(0,84,56);
+    ellipse(250, 250,40);
+
+//円7：赤
+　stroke(255);
+    fill(192,0,0);
+    ellipse(250, 250,20);
+    
+    
+  }
+  
 }
