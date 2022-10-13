bbdce67fd1e301943d9f3d490b445e634f78a06f
1880881h <115515285+1880881h@users.noreply.github.com>
2022-10-13 12:52:28 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,19 @@
-// 小手調べ
-function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+function setup() {
+createCanvas(100,100);
+background(255,255,255);
+
+  for(let i = 1; i < 11; i++){
+    
+if(i<5){
+let x = 100 - (10 *(i-1) );
+ellipse(50, 50, x);
+stroke(255, 0, 0);}
+  
+else if (5 < i <11 ){
+let x = 100 - (10 *(i-1) );
+ellipse(50, 50, x);
+stroke(0, 0, 255);
+}
+
+}
 }
